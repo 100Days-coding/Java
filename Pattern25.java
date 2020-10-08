34cb17b0c16086ae76b0857228ccfd1a03e9a606
kolagadisuresh1977 <71330112+kolagadisuresh1977@users.noreply.github.com>
2020-10-08 09:08:06 +0530

    Add files via upload

---
+++ Pattern25.java
@@ -0,0 +1,19 @@
+package Patterns2;
+public class Pattern25 {
+	public static void main(String[] args) {
+		int n =4;
+		
+		for(int i=1; i<=n; i++) // i = 1,2,3. 4 rows
+		{
+		 for(int j =n-1; j>= i; j--) // j = 3..0		
+			System.out.print(" "); // 4 spaces
+									
+		 for(int k=1; k >= i; k-- ) // k =1,3,5,7,9
+			System.out.print()(Math.abs(k-m)+65));  // BAB
+		  // (1-2)= 1+65=66, 2-2=0+65 =65, 3-2= 1+65 = 66	 		 
+								
+		System.out.println();
+		x= x+2; // x = 3,5,7,9		
+		m++; // m = 2,3
+		}
+}}
